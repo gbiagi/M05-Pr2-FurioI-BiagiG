bd7d0239ef71574e885ef38bb570e2363c2bba82
gbiagi <gbiagiharreguy.cf@iesesteveterradas.cat>
2022-12-07 17:35:43 +0100

    Creacio opc 3

--- M05-pr2.py
+++ M05-pr2.py
@@ -1,23 +1,33 @@
 menu_00 = "1.Introdueix una frase\n2.Elimina les vocals\n3.Elimina les consonants\n" \
           "4.Ordena la frase al revés i printala per pantalla\n5.Ordena les paraules de la frase en orde ascendent i printales per pantalla"
 flg_00 = True
-frase = ""
+frase = "Frase prueba"
 #alum2
 while flg_00:
     print(menu_00)
     opc = input("Elige una opcion: ")
     if not opc.isdigit():
         print("Solo se pueden introducir numeros.")
-    elif not int(opc) in range(0, 6):
+    elif not int(opc) in range(1, 6):
         print("Opcion incorrecta.")
     else:
-         if opc == 1:
-             print()
-         if opc == 2:
-             print()
-         if opc == 3:
-             print("Elimina les consonants")
-         if opc == 4:
-             print()
-         if opc == 5:
-             print("Ordena les paraules de la frase en orde ascendent i printales per pantalla")
+        opc = int(opc)
+        if opc == 1:
+            print()
+        if opc == 2:
+            print()
+        if opc == 3:
+            print("Elimina les consonants")
+            vocales = "aeiou "
+            nueva_frase = ""
+            for i in frase:
+                if i not in vocales:
+                    continue
+                else:
+                    nueva_frase += i
+            print(nueva_frase)
+            input("Enter to continue")
+        if opc == 4:
+            print()
+        if opc == 5:
+            print("Ordena les paraules de la frase en orde ascendent i printales per pantalla")
